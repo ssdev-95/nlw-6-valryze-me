b756194a433a70de36c45f217afb7169c3d904a7
xSallus <souza95salomao@gmail.com>
2021-06-22 11:05:39 -0400

    [UPDATE] Project structure data

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,9 @@
+import 'reflect-metadata';
 import express from 'express';
+import './database';
 
 const app = express();
 
-//HTTP Methods
-//get, post, put, delete, patch
-app.get('/', (req, res)=>res.send('Ola nlw'))
-app.post('/test/post', (req, res)=>res.send('Ola metodo POST'))
+app.listen(9000, () => console.log('Server is running on Port 9000'))
 
-//Server is set up to run at http://localhost:6000
-app.listen(9000, ()=>console.log('Server is running on Port 9000'))
+export default app;
