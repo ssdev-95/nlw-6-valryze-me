0e839558ca452dd510da438da1b59bf35916c412
xSallus <souza95salomao@gmail.com>
2021-06-22 12:10:35 -0400

    [UPDATE] Database handlers

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,15 @@
+import { CreateUserService } from '../services/CreateUserService';
+import { Request, Response } from 'express';
+
+class CreateUserController {
+    async handle(req: Request, res: Response) {
+        const { name, email, admin } = req.body;
+
+        const createUserService = new CreateUserService();
+        const user = await createUserService.execute({name, email, admin});
+
+        return res.json(user);
+    };
+};
+
+export { CreateUserController };
---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from "../repositories/UsersRepositories"
+import { IUser } from "../types"
+
+class CreateUserService {
+    async execute({name, email, admin}: IUser) {
+        const usersRepository = getCustomRepository(UsersRepositories);
+
+        if(!email) {
+            throw new Error('Email not found')
+        };
+
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists) {
+            throw new Error('User already exists!')
+        };
+
+        const user = usersRepository.create({
+            name,
+            email,
+            admin
+        });
+
+        await usersRepository.save(user);
+
+        return user;
+    }
+};
+
+export { CreateUserService }
