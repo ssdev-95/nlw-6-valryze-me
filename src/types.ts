5713390a93ba9c9e1f2b4df95f56a55bbcb47842
xSallus <souza95salomao@gmail.com>
2021-06-24 12:52:45 -0400

    [UPDATE] Types

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,17 @@ export interface IUser {
     name: string;
     email: string;
     admin?: boolean;
+    password: string;
+}
+
+export interface IUserAuthServiceProps {
+    email: string;
+    password: string;
+}
+
+export interface IComplimentServiceProps {
+    tag_id: string;
+    user_sender: string;
+    user_receiver: string;
+    message: string;
 }
