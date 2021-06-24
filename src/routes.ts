9ccab8cd584bdead3b3e001ca0025145877b5ce6
xSallus <souza95salomao@gmail.com>
2021-06-24 12:59:39 -0400

    [UPDATE] Some refs

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from 'express';
+import { AuthUserController } from './controllers/AuthUserController';
+import { CreateComplimentController } from './controllers/CreateComplimentController';
 import { CreateUserController } from './controllers/CreateUserController';
-import { CreateTagController } from './controllers/CreayeTagController';
+import { CreateTagController } from './controllers/CreateTagController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+const authUserController = new AuthUserController();
+const complimentController = new CreateComplimentController();
 
 router.post('/users/create', createUserController.handle);
 router.post('/tags/create', ensureAdmin, createTagController.handle);
+router.post('/user/auth', authUserController.handle);
+router.post('/compliments/create', complimentController.handle);
 
 export { router };
