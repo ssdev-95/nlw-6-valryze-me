8f038e1088fcb26edb692649fb1b6546ceb79739
xSallus <souza95salomao@gmail.com>
2021-09-12 11:58:34 -0400

    [UPDATE] Rota que lista todos os usuarios

--- src/controllers/GetUsersController.ts
+++ src/controllers/GetUsersController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
+import { GetUsersService } from '..//services/GetUsersService';
 
 class GetUsersController {
     async handle(req:Request, res:Response) {
         const { authorization } = req.headers;
+        const getUsersService = new GetUsersService();
 
-        if (authorization) {
-            return res.status(200).json({ status: 'Authorized' });
+        const users = await getUsersService.execute();
+
+        if (!authorization || authorization.trim()==="") {
+            return res.status(300).json({ status: 'Unauthorized' });
         }
 
-        return res.status(300).json({ status: 'Unauthorized' });
+        return res.status(200).json({ users: users });
     }
 }
 
--- src/services/GetUsersService.ts
+++ src/services/GetUsersService.ts
@@ -1,5 +1,32 @@
+import { collection, db, getDocs } from '../database/firebase';
+
 class GetUsersService {
-    async execute() {}
+  async execute() {
+    let users:any[] = [];
+    try {
+      const docsRef = await getDocs(collection(db, 'users'));
+
+      if(!docsRef) {
+        throw new Error('Something went wrong');
+      }
+
+      docsRef.forEach(doc=>{
+        users.push({
+            id: doc.id,
+            name: doc.data().name,
+            email: doc.data().email,
+            admin: doc.data().admin
+        })
+      });
+    }
+    catch (e) {
+      if (e instanceof Error) {
+        throw new Error(e.message);
+      }
+    }
+
+    return users;
+  }
 }
 
 export { GetUsersService };
