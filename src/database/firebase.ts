29ce62d8ed74b9ee4c397cdbe53963701fa6115a
Salomao Souza <58428963+xSallus@users.noreply.github.com>
2021-09-17 12:10:01 -0400

    kkkkkkk

--- src/database/firebase.ts
+++ src/database/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore, collection, addDoc, getDocs } from 'firebase/firestore';
 require('dotenv').config();
 
 const config = {
-  apiKey: process.env.API_KEY, // "AIzaSyAoM4LThh-Ga8vmxuWFZLd0cQxWDfZaMfw"
-  authDomain: process.env.AUTH_DOMAIN, // "ecoleta-beta.firebaseapp.com"
-  projectId: process.env.PROJECT_ID, // "ecoleta-beta"
-  storageBucket: process.env.STORAGE_BUCKET, // "ecoleta-beta.appspot.com"
-  messagingSenderId: process.env.MSG_SNDER_ID, // "867897420568"
-  appId: process.env.APP_ID // "1:867897420568:web:0b6fe36e17ff88f9bf0c6d"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MSG_SNDER_ID,
+  appId: process.env.APP_ID
 };
 
 const firebaseApp = initializeApp(config);
